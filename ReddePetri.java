e33d142a613d3e860f814c5f2b4d7d73808989ee
diegomirandahenriquez <32444316+diegomirandahenriquez@users.noreply.github.com>
2017-11-27 01:32:33 -0300

    Update ReddePetri.java

--- ReddePetri.java
+++ ReddePetri.java
@@ -1,27 +1,29 @@
 public class ReddePetri{
 	
+	/*MODELAMIENTO DE UNA RED DE PETRI DE EL CICLO DE DESARROLLO DE SOFTWARE*/
+	
 	public static void main(String[] args) throws InterruptedException {
 		while(true){
-			Hilo analista = new Hilo("Analista");
-			Hilo diseñador = new Hilo("Diseñador");
+			Hilo analista = new Hilo("Analista");  			/*crea el hilo de analista*/
+			Hilo diseñador = new Hilo("Diseñador");			/*crea el hilo de diseñador*/
 			
-			analista.start();
-			diseñador.start();
-			analista.join();
-			diseñador.join();
+			analista.start();					/*corre al analista*/
+			diseñador.start();					/*corre al diseñador*/
+			analista.join();					/*espera al analista*/
+			diseñador.join();					/*espera al diseñador*/
 			
-			Hilo programador1 = new Hilo("Programador1");
-			Hilo programador2 = new Hilo("Programador2");
+			Hilo programador1 = new Hilo("Programador1");		/*crea el hilo de programador 1*/
+			Hilo programador2 = new Hilo("Programador2");		/*crea el hilo de programador 2*/
 			
-			programador1.start();
-			programador2.start();
-			programador1.join();
-			programador2.join();
+			programador1.start();					/*corre al programador 1*/
+			programador2.start();					/*corre al programador 2*/
+			programador1.join();					/*espera al programador 1*/
+			programador2.join();					/*espera al programador 2*/
 			
-			Hilo testing = new Hilo("Testing");
+			Hilo testing = new Hilo("Testing");			/*crea el hilo de testing*/
 			
-			testing.start();
-			testing.join();
+			testing.start();					/*corre al testing*/
+			testing.join();						/*espera al testing*/
 		}
 	}
 }
